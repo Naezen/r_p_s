197f4129ae4de2f4778f1dc31f7d7d0cb377e199
Naezen <141554795+Naezen@users.noreply.github.com>
2023-11-05 20:57:17 +0000

    add r_p_s player-computer choice-choice comparison
    player input, computer random, choice string comparison, console log

--- r_p_s.js
+++ r_p_s.js
@@ -1,39 +1,37 @@
+//function game()
+//function getComputerChoice
+//function getPlayerChoice
+//function playRound
+//function to compare choices
+//score count
 
+function getPlayerChoice() {
+   let playerInput = prompt("Choose rock, paper or scissors".toLowerCase());
+   return playerInput;
+};
+console.log("You chose " + prompt());
 
-
-// function cpu random choice to return rock, paper or scissors
-// Math.random
-// compare values to rock, paper or scissors
-// if <1 - rock, if <2 - paper, if <3 then scissors
-
-//function user input choice to choose rock, paper or scissors
-
-//rock is < paper, paper is < scissors, scissors is < rock
-
-
-// list of choices
-let computerSelection = [0, 1, 2];
-// player choice input
-let playerSelection = prompt("rock, paper or scissors"); 
-
-console.log("You chose " + playerSelection.toLowerCase());
-
-// computer choice random
 function getComputerChoice() {
-   return Math.floor(Math.random()*3);
-}
-
-if (getComputerChoice([]) === 0) {
-   computerSelection = "rock";
-} else {
-   if (getComputerChoice([]) === 1) {
-      computerSelection = "paper";
-   } else {
-      computerSelection = "scissors";
+   const randomChoice = (Math.floor(Math.random() * 3));
+   if (randomChoice === 0 ) {
+      return "rock";
+   } else if (randomChoice === 1 ) {
+      return "paper";
+   } else if (randomChoice === 2 ) {
+      return "scissors";
    }
 };
-console.log("Computer chose " + computerSelection.toLowerCase());
+console.log("Computer chose " + getComputerChoice());
 
 
-// choice comparison
-// declare win, lose, draw
+function compareChoices(playerChoice, computerChoice) {
+   if (playerChoice === computerChoice) {
+      return "Draw";
+   }
+   if ((playerChoice === 'rock' && computerChoice === 'scissors') ||
+      (playerChoice === 'scissors' && computerChoice === 'paper') ||
+      (playerChoice === 'paper' && computerChoice === 'rock')) {
+      return "You win";
+   } 
+   return "Computer wins";
+};
